6c77b0e82650406e87eecd93b5cc8f8de9d48b42
yishiwang331 <112493799+HjxhJZhx@users.noreply.github.com>
2022-09-23 16:40:05 +0000

    reply

---
+++ README.md
@@ -0,0 +1,4 @@
+# CS110 Portfolio
+Preferred Name: <<yishi wang>>
+Discord username: <<yishi（rocky）wang#1517>>
+REPL url: <<https://replit.com/join/adlryqhibm-hjxhjzhx>>
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -4,7 +4,8 @@ import random
 #Part A
 window = turtle.Screen() # 2.  Create a screen
 window.bgcolor('lightblue')
-
+x = random.randrange(1,10)
+print(x)
 michelangelo = turtle.Turtle() # 3.  Create two turtles
 leonardo = turtle.Turtle()
 michelangelo.color('orange')
@@ -18,9 +19,13 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-
-
+x = random.randrange(1,10)
+print(x)
 # PART B - complete part B here
+import pygame
+import math 
 
+pygame.init()
+window = pygame.display.set_mode
 
 window.exitonclick()
