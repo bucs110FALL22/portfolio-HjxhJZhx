05fa64034fa2c89f1c5344c151da30327f68408e
yishiwang331 <112493799+HjxhJZhx@users.noreply.github.com>
2022-09-28 02:56:07 +0000

    I made the program repeat 10 times to chase each other.

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -19,13 +19,41 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-x = random.randrange(1,10)
-print(x)
+x=1
+loop while x<11
+x=x+1
+result = random.randrange(1,100)
+print(result)
+michelangelo.forward(result)
+number = random.randrange(1,100)
+leonardo.froward(result)
+michelangelo.up() 
+leonardo.up()
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 # PART B - complete part B here
-import pygame
-import math 
-
 pygame.init()
 window = pygame.display.set_mode
+import pygame
+import math 
+import turtle 
+for i in range(3)
+  turtle.seth(i*120)
+  turtle.fd(100)
+turtle.done()
+for i in range(0,rows):
+  for k in range(0,rows):
+    print"x",
+    k += 1
+  i +=1
+print "/n"
+turtle.done() 
 
+n = 8
+x = (n-2)*180/n
+turtle.speed(1)
+turtle.pensize(2)
+for_in range (n)
+turtle.forward(100)
+turtle.done
 window.exitonclick()
