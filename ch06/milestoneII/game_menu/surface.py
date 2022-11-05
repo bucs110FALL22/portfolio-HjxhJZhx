508ade2ed50fe2f34fa7104b9034c2ce6e05a614
yishiwang331 <112493799+HjxhJZhx@users.noreply.github.com>
2022-11-05 02:18:38 +0000

    1234

--- ch06/milestoneII/game_menu/surface.py
+++ ch06/milestoneII/game_menu/surface.py
@@ -2,6 +2,6 @@ from Rectangle import Rectangle
 class Surface:
   def __init__(self,filename,x,y,h,w):
    self.rect = Rectangle(x,y,h,w) 
-    self.rect = Rectangle(x,y,h,w)
+  self.image = filename
   def.getRect(self):
     return self.rect
