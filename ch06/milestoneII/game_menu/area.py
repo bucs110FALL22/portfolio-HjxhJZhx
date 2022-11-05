584188393743fdc0aba4f9d523bf38a7220d4542
yishiwang331 <112493799+HjxhJZhx@users.noreply.github.com>
2022-11-05 02:25:35 +0000

    123456

--- ch06/milestoneII/game_menu/area.py
+++ ch06/milestoneII/game_menu/area.py
@@ -1,9 +1,11 @@
 class Rectangle:
   def__init__(self, x=0,y=0,h=0,w=0):
   if x<0:
-      x=0
+    x=0
   if y<0:
     y=0
+  if h<0:
+    h=0
   if w<0
     w=0
   self.x = x
@@ -12,5 +14,5 @@ self.height = h
 self.width = w
 
 def __str__(self):
-  return(f*(x:,{self.x}),y:{self.y},width)
+  return(f"(x:,{self.x}),y:{self.y},width)
 {self.width},height:{self.height}")
