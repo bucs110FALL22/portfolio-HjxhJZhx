3eb3c7110261a7aa55f87231daee4c50afacf3f8
yishiwang331 <112493799+HjxhJZhx@users.noreply.github.com>
2022-12-10 05:42:18 +0000

    final project

--- README.md ProgramDESIGN/game end menu.py
+++ README.md ProgramDESIGN/game end menu.py
@@ -1,27 +1,31 @@
 import pygame
 import sys
 
+pygame.init()
+
 gameover_font = pygame.font.Font("font/font.ttf",48)
-again_image = pygame.image.load("images/again.png").convert_alfa()
-gameover_image = pygame.image.load("images/gameover.png").convert_alfa()
+again_image = pygame.image.load("images/again.png").convert_alpha()
+gameover_image = pygame.image.load("images/gameover.png").convert_alpha()
+
+wrong_matches = []
 
 if match_NUM > 0:
-  for each in wrong matches:
-  if each.active ==True:
-    each.move()
-    screem.blit(each.image,each.rect)
+  for each in wrong_matches:
+    if each.active ==True:
+      each.move()
+      screem.blit(each.image,each.rect)
     
 pygame.mixer.music.stop()
 
 gameover_socre = gameover_font.render("Score : %s" % str(score), True, Whitefont())
-screen.blit(gameover_score.(100,200)) 
+screen.blit(gameover_score,(100,200)) 
 screen.blit(again_image,(90,350))
 screen.blit(gameover_image,(90,450))
-mouse_down = pygame.mouse .get pressed()
+mouse_down = pygame.mouse .get_pressed()
 if mouse_down[0]:
     pos = pygame.mouse.get_pos()
     if 90 < pos[0] < 390 and 350 < pos[1] < 390:
       main()
-    elif 90 < pos[0] < 390 adn 450 < pos[1] < 490:
+    elif 90 < pos[0] < 390 and 450 < pos[1] < 490:
           pygame.quit
           sys.exit()
--- README.md ProgramDESIGN/surface.py
+++ README.md ProgramDESIGN/surface.py
@@ -3,5 +3,5 @@ class Surface:
   def __init__(self,filename,x,y,h,w):
     self.rect = (Rectangle(x,y,h,w))
     self.image = filename
-  def.getRect(self):
+  def getRect(self):
     return self.rect
